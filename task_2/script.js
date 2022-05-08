ba81df016192a2fb9d70e0cc92da2fdb400520d8
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-05-08 17:00:28 +0300

    proceed

--- task_2/script.js
+++ task_2/script.js
@@ -4,4 +4,14 @@
 Ваша задача — создать JS-объект, который при преобразовании в JSON будет возвращать в качестве результата такую же JSON-строку, как в образце. Получившуюся строку вывести в консоль.
 Удачи! */
 
-let jsonString = {"name":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000}
+const person = {
+    "name": "Anton",
+    "age": 36,
+    "skills": ["Javascript",
+        "HTML",
+        "CSS"],
+    "salary": 80000
+};
+
+const jsonPerson = JSON.stringify(person);
+console.log(jsonPerson);
